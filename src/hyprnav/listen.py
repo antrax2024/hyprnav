ac7bfefb9c17d4c1729fffadd364853cd4d0d10e
Antrax <fredcox@gmail.com>
2025-04-22 22:49:33 -0300

    shutup sound

--- src/hyprnav/listen.py
+++ src/hyprnav/listen.py
@@ -4,7 +4,7 @@ from .window import showWorkspaceWindow
 instance = Hyprland()
 
 
-def on_workspace_changed(sender, **kwargs) -> None:
+def onWorkspaceChanged(sender, **kwargs) -> None:
     workspace_name = kwargs.get("workspace_name")
     print(kwargs)
     print(f"Workspace changed: {workspace_name}")
@@ -13,6 +13,10 @@ def on_workspace_changed(sender, **kwargs) -> None:
 
 def listen() -> None:
     """Listen for workspace changes and show a window."""
-    # Connect to the Hyprland signals
-    instance.signals.workspacev2.connect(on_workspace_changed)
-    instance.watch()
+    try:
+        # Connect to the Hyprland signals
+        instance.signals.workspacev2.connect(onWorkspaceChanged)
+        instance.watch()
+    except KeyboardInterrupt:
+        print("Interrupt by user. Exiting...")
+        return
