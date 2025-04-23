8b1098bc18a919b6b513f53c4a1e1e65d7e34f92
Antrax <fredcox@gmail.com>
2025-04-22 22:11:37 -0300

    first commit

---
+++ src/hyprnav/window.py
@@ -0,0 +1,31 @@
+import sys
+from PyQt6 import QtWidgets, uic
+import importlib.resources
+from PyQt6.QtGui import QGuiApplication
+
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self) -> None:
+        super().__init__()
+        # Load ui
+        uiFile = importlib.resources.files("hyprnav").joinpath("assets/window.ui")
+        uic.loadUi(uiFile, self)  # type: ignore
+
+        # Set application name and class before creating QWidget
+        QGuiApplication.setApplicationName("hyprnav")
+        QGuiApplication.setDesktopFileName("hyprnav")
+        QGuiApplication.setApplicationDisplayName("hyprnav")
+
+        # widget access
+        self.workspaceLabel.setText(":: Gonha ::")  # type: ignore
+
+        self.show()
+
+    # def buttonClicked(self):
+    #     print("Button clicked!")
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    window = MainWindow()
+    sys.exit(app.exec())
