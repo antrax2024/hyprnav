591fe4e6f6b4603868a2e3bb0a344589ea10699e
Antrax <fredcox@gmail.com>
2025-05-14 21:10:41 -0300

    remove test.py

--- test.py
+++
@@ -1,4 +0,0 @@
-from playsound3 import AVAILABLE_BACKENDS, DEFAULT_BACKEND
-
-print(AVAILABLE_BACKENDS)  # for example: ["gstreamer", "ffmpeg", ...]
-print(DEFAULT_BACKEND)  # for example: "gstreamer"
