e3f333e2a35535d64f6bcca66fb23473807bac78
Antrax <fredcox@gmail.com>
2025-05-24 08:38:03 -0300

    everything ok, now i will edit and modify README.md

--- src/hyprnav/config.py
+++ src/hyprnav/config.py
@@ -1,3 +1,4 @@
+import sys
 import os
 import shutil
 from typing import Any
@@ -5,6 +6,8 @@ from confz import BaseConfig, FileSource
 from .constants import APP_NAME, APP_VERSION, CONFIG_FILE, STYLE_FILE
 import importlib.resources
 from rich.console import Console
+from rich.table import Table
+from hyprnav.util import fileExists, showError
 
 cl = Console()
 
@@ -14,55 +17,6 @@ def printMessage(preamble: str, variable: Any) -> None:
     Print a message to the console with fixed width preamble.
     """
     cl.print(f"[bold yellow]{preamble:<15}[/bold yellow]: {variable}")
-
-
-def ensureConfigFiles() -> None:
-    """
-    Ensure that all required configuration files exist.
-    This function must be called before any AppConfig instances are created.
-    """
-    # Check if the config file exists
-    checkFile(file=CONFIG_FILE)
-    # Check if the style file exists
-    checkFile(file=STYLE_FILE)
-
-
-def cli() -> None:
-    cl.print(f"[bold yellow]{APP_NAME} v{APP_VERSION}[/bold yellow]\n")
-    cl.print("[cyan]=[/cyan]" * 80)
-    # Display configuration information
-    printMessage(preamble="Config", variable=CONFIG_FILE)
-    printMessage(preamble="Style", variable=STYLE_FILE)
-    # Check if sound if Enabled
-    appConfig = AppConfig()
-    printMessage(
-        preamble="Audio File",
-        variable=(f"{appConfig.sound.file}"),
-    )
-    printMessage(
-        preamble="Enabled",
-        variable=("[green]Yes[/green]" if appConfig.sound.enabled else "[red]No[/red]"),
-    )
-
-    cl.print("[cyan]=[/cyan]" * 80)
-
-
-def checkFile(file: str) -> None:
-    if not os.path.exists(path=file):
-        os.makedirs(name=os.path.dirname(p=file), exist_ok=True)
-        copyFile(destination=file)
-
-
-def copyFile(destination) -> None:
-    # Extract only the filename from destination variable
-    filename = os.path.basename(destination)
-
-    source = importlib.resources.files(anchor="hyprnav").joinpath(f"assets/{filename}")
-    # Convert Traversable to string path and copy the file
-    shutil.copy2(
-        src=str(object=source),
-        dst=destination,
-    )
 
 
 class Sound(BaseConfig):
