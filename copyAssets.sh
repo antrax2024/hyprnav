7aba4d6d27472e9dd34eb0c667b2a23f2ec0683c
Antrax <fredcox@gmail.com>
2025-05-24 09:44:55 -0300

    Release v0.1.8

--- copyAssets.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-cp ./src/hyprnav/assets/config.yaml ~/.config/hyprnav/
-cp ./src/hyprnav/assets/style.css ~/.config/hyprnav/
-
-echo "Assets copied to ~/.config/hyprnav/"
--- src/hyprnav/constants.py
+++ src/hyprnav/constants.py
@@ -1,7 +1,7 @@
 import os
 
 # This is the version of the application.
-APP_VERSION = "0.1.6"
+APP_VERSION = "0.1.8"
 # This is the name of the application.
 APP_NAME = "hyprnav"
 
